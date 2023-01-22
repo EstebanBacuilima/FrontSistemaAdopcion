8097af7225984cb3f395be05b68b9f2e8247fcca
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-01-21 19:05:32 -0500

    New
    New structute frontend

---
+++ src/app/Modules/admin-fundacion/admin-fundacion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminFundacionRoutingModule } from './admin-fundacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminFundacionRoutingModule
+  ]
+})
+export class AdminFundacionModule { }
---
+++ src/app/Modules/visitantes/visitantes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VisitantesRoutingModule } from './visitantes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VisitantesRoutingModule
+  ]
+})
+export class VisitantesModule { }

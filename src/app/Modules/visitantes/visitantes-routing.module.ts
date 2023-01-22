62b99f2be3994ef255fadec923ba1afb4dee6782
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-01-21 19:15:47 -0500

    Aplicado
    Rutas del primer Modulo

--- src/app/Modules/visitantes/visitantes-routing.module.ts
+++ src/app/Modules/visitantes/visitantes-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import("./login/login.component").then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import("./register/register.component").then(m => m.RegisterComponent)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import("./welcome/welcome.component").then(m => m.WelcomeComponent)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sup-admin',
+    loadChildren: () => import('./Modules/super-admin/super-admin.module').then(m => m.SuperAdminModule) 
+  },
+  {
+    path: 'admin-fundacion',
+    loadChildren: () => import('./Modules/admin-fundacion/admin-fundacion.module').then(m => m.AdminFundacionModule) 
+  },
+  {
+    path: 'clientes',
+    loadChildren: () => import('./Modules/cliente/cliente.module').then(m => m.ClienteModule) 
+
+  },
+  {
+    path: 'visitantes',
+    loadChildren: () => import('./Modules/cliente/cliente.module').then(m => m.ClienteModule) 
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}

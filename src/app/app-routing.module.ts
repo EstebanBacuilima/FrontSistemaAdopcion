d3a086732ea69d1004d926ac6c0d93410c6a77fe
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-01-21 20:37:50 -0500

    Listo
    Go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Modules/visitantes/login/login.component';
+import { WelcomeComponent } from './Modules/visitantes/welcome/welcome.component';
+
 
 const routes: Routes = [
-  {
-    path: 'sup-admin',
-    loadChildren: () => import('./Modules/super-admin/super-admin.module').then(m => m.SuperAdminModule) 
-  },
-  {
-    path: 'admin-fundacion',
-    loadChildren: () => import('./Modules/admin-fundacion/admin-fundacion.module').then(m => m.AdminFundacionModule) 
-  },
-  {
-    path: 'clientes',
-    loadChildren: () => import('./Modules/cliente/cliente.module').then(m => m.ClienteModule) 
-
-  },
-  {
-    path: 'visitantes',
-    loadChildren: () => import('./Modules/cliente/cliente.module').then(m => m.ClienteModule) 
-  },
-  
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { HeaderComponent } from './Components/header/header.component';
-import { BienvenidoComponent } from './Modules/bienvenido/bienvenido.component';
-import { WelcomeComponent } from './Modules/Visitante/welcome/welcome.component';
-import { ListMascotasComponent } from './Modules/Visitante/list-mascotas/list-mascotas.component';
-import { ListFundacionesComponent } from './Modules/Visitante/list-fundaciones/list-fundaciones.component';
-import { LoginComponent } from './Modules/Visitante/login/login.component';
-import { RegisterComponent } from './Modules/Visitante/register/register.component';
+import { LoginComponent } from './Modules/visitantes/login/login.component';
+import { WelcomeComponent } from './Modules/visitantes/welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    BienvenidoComponent,
-    WelcomeComponent,
-    ListMascotasComponent,
-    ListFundacionesComponent,
     LoginComponent,
-    RegisterComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,

060dd7e83fb7c7cf5feaea9f8a2dd0e3085cffa3
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-02-02 20:19:51 -0500

    new
    cahnngees

---
+++ src/app/Models/SolicitudAdopcion.ts
@@ -0,0 +1,13 @@
+import { Mascota } from "./Mascota";
+import { Usuario } from "./Usuario";
+
+export class SolicitudAdopcion {
+
+    idSolicitudAdopcion?: number;
+    fecha_solicitud_adopcion?: Date;
+    descripcion?: string;
+    estado?: boolean;
+
+    mascota?: Mascota;
+    usuario?: Usuario;
+} 

f9a0fbaff6e554d1bd3210b58ef04b617f7ea7ee
danyCP <101678742+danyCP@users.noreply.github.com>
2023-02-25 11:13:39 -0500

    up List Fu
    up List Fu

--- src/app/Modules/admin-super/list-fundacio/list-fundacio.component.ts
+++ src/app/Modules/admin-super/list-fundacio/list-fundacio.component.ts
@@ -249,20 +249,84 @@ export class ListFundacioComponent implements OnInit {
   fechaAct: Date =new Date();
 
   // PDF
+
+  generarPDF() {
+    const data = this.listaFundaciones;
+    const body = [];
+
+    body.push(["ID", "RUC", "NOMBRE FUDACION", "ACRO", "MISION", "DIRECCION", "CORREO", "TELEFONO"]);
+
+    data.forEach(fundacion => {
+      body.push([fundacion.idFundacion, fundacion.ruc, fundacion.nombre_fundacion, fundacion.acronimo, fundacion.mision, fundacion.direccion, fundacion.correo, fundacion.telefono]);
+    });
+
+    const table = {
+      text: 'Tables',
+      headerRows: 1,
+      body,
+      layout: "lightHorizontalLines",
+      fillColor: '#eeffee',
+      widths: [12, 70, 70, 30, 70, 70, 60, 55]
+    };
+
+    const styles: any = {
+      header: {
+        text: 'Tables',
+        bold: true,
+        fontSize: 8,
+        color: "#a9cbff",
+        background: 'lightblue',
+        font: "Roboto-Regular.ttf",
+        margin: [0, 20, 0, 10]
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 5,
+        color: "#a9cbff",
+        background: 'lightblue',
+        fillColor: '#a9cbff',
+        font: "Roboto-Regular.ttf"
+      },
+    };
+
+    const cuerpo = [{
+      text: "Título del PDF",
+      style: "header",
+      margin: [0, 0, 0, 20]
+    },
+    ];
+
+    const content = [
+      {
+        cuerpo,
+        table,
+        style: "tableExample"
+      }];
+
+    const documentDefinition = {
+      content,
+      styles,
+      layout: 'lightHorizontalLines',
+    };
+
+    pdfMake.createPdf(documentDefinition).open();
+  }
+
+
   openPdfTables() {
     let fechaPrueba: Date = new Date();
     let fechaFormateada = fechaPrueba.toISOString().substr(0,10);
     console.log("es la fecha de hoy -> " + fechaFormateada);
     let tableBody = [];
     tableBody.push([
-      { text: "ID", bold: true },
-      { text: "RUC", bold: true },
-      { text: "FUNDACION", bold: true },
-      { text: "ACRONIMO", bold: true },
-      { text: "MISION", bold: true },
-      { text: "DIRECCION", bold: true },
-      { text: "CORREO", bold: true },
-      { text: "TELEFONO", bold: true },
+      { text: "ID", bold: true, background: 'lightblue' },
+      { text: "RUC", bold: true, background: 'lightblue'},
+      { text: "FUNDACION", bold: true, background: 'lightblue'},
+      { text: "ACRONIMO", bold: true, background: 'lightblue'},
+      { text: "MISION", bold: true, background: 'lightblue'},
+      { text: "DIRECCION", bold: true, background: 'lightblue'},
+      { text: "CORREO", bold: true, background: 'lightblue'},
+      { text: "TELEFONO", bold: true, background: 'lightblue'},
     ]);
     this.listaFundaciones.forEach(fundacion => {
       let fila = [];
